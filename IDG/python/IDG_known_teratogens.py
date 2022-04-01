6bc943519c9a63962d283523323ed5eefa04e350
Jess Binder <40614665+JessBinder@users.noreply.github.com>
2022-04-01 08:42:39 -0600

    updated DrugCentral relationships

--- IDG/python/IDG_known_teratogens.py
+++ IDG/python/IDG_known_teratogens.py
@@ -18,16 +18,17 @@ input_file = csv.DictReader(open("input/IDG_known_SBD_teratogens.csv"))
 
 g = Graph()
 
-do = Namespace("https://www.ebi.ac.uk/spot/oxo/terms/")
+DC_ID = Namespace("https://drugcentral.org/drugcard/")
 subject = Namespace("https://identifiers.org/pubchem.compound/")
 
-g.bind('compound',subject)
-
+g.bind('PubChem_CID',subject)
+g.bind('DrugCentral_ID',DC_ID)
 
 for row in input_file:
     Drug_URI = subject + str(row['PubChem_CID'])
+    DC_URI = DC_ID + str(row['DC_ID'])
     row = dict(row)
     g.add((URIRef(Drug_URI), RDFS.label, Literal(row['Curated_known_teratogens'])) )
-    g.add((URIRef(Drug_URI), RDFS.label, Literal('DC_ID: ' + row['DC_ID'])) )
+    g.add((URIRef(Drug_URI), RDF.type, URIRef(DC_URI)))
 
 g.serialize(format="turtle",destination='IDG_known_SBD_teratogens.ttl')
